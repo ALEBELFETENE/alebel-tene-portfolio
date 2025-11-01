0693cfea89893e460f31784beda6512e4e8fe218
alamirew wagaw <akliluarse@gmail.com>
2025-11-01 12:00:06 -0700

    Setup Tailwind + Vite for Vercel

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,4 +9,18 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+}/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./public/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
 }
